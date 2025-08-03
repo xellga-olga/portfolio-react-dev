5c7ebcfe0ec7d0a4e27059229a8bb0672ff333a0
Noskina Olha <noskinaolga1@gmail.com>
2025-08-03 16:24:18 +0200

    add Welcome screen for QuizApp

--- src/mini-apps/QuizApp/QuizApp.jsx
+++ src/mini-apps/QuizApp/QuizApp.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Welcome from "./welcome";
 import QuizQuestion from "./quizQuestion";
 import Result from '../QuizApp/result'
+import questions from "./questions";
 
 function QuizApp() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  const handleAnswer = (isCorrect) => {
+    const newAnswers = [...answers, isCorrect ? 1 : 0];
+
+    setAnswers(newAnswers);
+
+    if(currentQuestion >= questions.length - 1) {
+      setShowResult(true)
+    }else {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+  }
+
+  const startTest = () => setShowWelcome(false);
+
+  const restartTest = () => {
+    setCurrentQuestion(0)
+    setAnswers([])
+    setShowResult(false)
+    setShowWelcome(true)
+  }
 
   return (
     <div
-      className="max-w-xl mx-auto mt-10 p-8 mb-7 bg-black/60 backdrop-blur-md shadow-2xl flex content-center flex-col items-center rounded-3xl text-center border border-white/50">
-      <h2 className="text-2xl font-bold mb-4 text-white/70">Quiz App</h2>
-      <Welcome />
-      <QuizQuestion />
-      <Result />
+      className="max-w-xl mx-auto mt-10 p-8 mb-7 bg-white  flex content-center flex-col items-center rounded-3xl text-center ">
+      <h2 className="text-2xl font-bold mb-4 text-black/70">Quiz App</h2>
+      {showWelcome && <Welcome onStart={startTest} />}
+
+      {!showWelcome && !showResult && (
+        <QuizQuestion
+        question={questions[currentQuestion]}
+        questionNumber={currentQuestion + 1}
+        onAnswer={handleAnswer}
+        />
+      )}
+
+      {showResult && <Result answers={answers} restartTest={restartTest} />}
     </div>
   );
 }
--- src/mini-apps/QuizApp/welcome.jsx
+++ src/mini-apps/QuizApp/welcome.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import reactLogo from '../../assets/mini-apps-images/reactLogo.jpg'
 
-function Welcome() {
+function Welcome({onStart}) {
   return (
-    <div></div>
+    <div className='flex flex-col items-center gap-6 text-center sm:gap-8'>
+      <img src={reactLogo} alt='React Logo' className='w-[150px] sm:w-[200px] sm:h-[200px]'/>
+      <h1 className='text-2xl font-bold text-gray-800 sm:text-4xl'>React Knowledge Quiz</h1>
+      <span className='text-lg font-semibold sm:text-3xl'>
+        Test your React expertise!
+      </span>
+      <button onClick={onStart} className='w-1/2 bg-black rounded-full text-white text-sm px-4 py-2 hover:opacity-80 sm:w-1/3 sm:text-xl '>Start</button>
+    </div>
   );
 }
 
